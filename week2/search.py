bdc5488c9e4fb258afc72ac91e5a78817365c599
Alex Kataev <alexander.kataev@wolt.com>
2023-02-20 12:52:04 +0000

    suggestions_and_autocomplete

--- week2/search.py
+++ week2/search.py
@@ -62,9 +62,22 @@ def autocomplete():
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
-            type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
+            _type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
+            type_to_index = {"products": "bbuy_products", "queries": "bbuy_queries"}
+            suggest = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion":{
+                            "field": "suggest",
+                            "skip_duplicates": True
+                        }
+                    }
+                }
+            }
+            index_name = type_to_index.get(_type)
+            search_response = get_opensearch().search(body=suggest, index=index_name)
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
@@ -106,7 +119,7 @@ def query():
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
         ##### W2, L1, S2
-
+        qu.add_spelling_suggestions(query_obj, user_query)
         ##### W2, L2, S2
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
@@ -121,7 +134,7 @@ def query():
             (filters, display_filters, applied_filters) = process_filters(filters_input)
         query_obj = qu.create_query(user_query,  filters, sort, sortDir, size=20)
         #### W2, L1, S2
-
+        qu.add_spelling_suggestions(query_obj, user_query)
         ##### W2, L2, S2
 
     else:
